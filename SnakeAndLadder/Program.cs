0035b1560bc47143962d998aeb8866f575ca8c54
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-13 12:28:45 +0530

    [Mukesh] add: created for snake and ladder problem

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLader
+{
+
+    internal class SnakeAndLader
+    {
+        int position = 0;
+    }
+}
