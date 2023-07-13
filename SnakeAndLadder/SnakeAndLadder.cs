6a1fb2371170aced94400cdbf0ea74b4b16011a6
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-13 12:58:57 +0530

    [Mukesh] add: new feature was added for finding position of die

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -1,15 +1,22 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SnakeAndLader
 {
-    
 
     internal class SnakeAndLader
     {
         int position = 0;
+        Random random = new Random();
+        public void DieRoll()
+        {
+            int diePosition = random.Next(1, 7);
+            Console.WriteLine("Die POsition is" + diePosition);
+        }
+
     }
 }
